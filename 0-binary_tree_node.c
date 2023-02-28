1afdc24fb90db4fd0210c56510d928960502b8e0
Natangwe <natangwetj@gmail.com>
2023-02-28 03:23:19 -0900

    fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -25,3 +25,79 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
     return (new_node);
 }
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ *
+ * @parent: pointer to the node to insert the left-child in
+ * @value: value to store in the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    /* Check if parent is NULL */
+    if (parent == NULL)
+        return (NULL);
+
+    /* Allocate memory for new node */
+    new_node = binary_tree_node(parent, value);
+    if (new_node == NULL)
+        return (NULL);
+
+    /* Insert new node as left-child */
+    if (parent->left != NULL)
+    {
+        parent->left->parent = new_node;
+        new_node->left = parent->left;
+    }
+    parent->left = new_node;
+
+    return (new_node);
+}
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ *
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    /* Check if parent is NULL */
+    if (parent == NULL)
+        return (NULL);
+
+    /* Allocate memory for new node */
+    new_node = binary_tree_node(parent, value);
+    if (new_node == NULL)
+        return (NULL);
+
+    /* Insert new node as right-child */
+    if (parent->right != NULL)
+    {
+        parent->right->parent = new_node;
+        new_node->right = parent->right;
+    }
+    parent->right = new_node;
+
+    return (new_node);
+}
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ *
+ * @tree: pointer to the root node of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree != NULL)
+    {
+        binary_tree_delete(tree->left);
+       
