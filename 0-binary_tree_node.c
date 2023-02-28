f14355d2a8bfd0f3538a4b659b8909ab76093d46
Natangwe <natangwetj@gmail.com>
2023-02-28 03:36:51 -0900

    0-fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,103 +1,120 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
+ * binary_tree_node - Creates a binary tree node
  *
- * @parent: pointer to the parent node of the node to create
- * @value: value to put in the new node
+ * @parent: Pointer to the parent node of the node to create
+ * @value: Value to put in the new node
  *
- * Return: pointer to the new node, or NULL on failure
+ * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Allocate memory for new node */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Initialize node */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	/* Initialize node */
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
  *
- * @parent: pointer to the node to insert the left-child in
- * @value: value to store in the new node
+ * @parent: Pointer to the node to insert the left-child in
+ * @value: Value to store in the new node
  *
- * Return: pointer to the new node, or NULL on failure
+ * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Check if parent is NULL */
-    if (parent == NULL)
-        return (NULL);
+	/* Check if parent is NULL */
+	if (parent == NULL)
+		return (NULL);
 
-    /* Allocate memory for new node */
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for new node */
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Insert new node as left-child */
-    if (parent->left != NULL)
-    {
-        parent->left->parent = new_node;
-        new_node->left = parent->left;
-    }
-    parent->left = new_node;
+	/* Insert new node as left-child */
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new_node;
+		new_node->left = parent->left;
+	}
+	parent->left = new_node;
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
- * binary_tree_insert_right - inserts a node as the right-child of another node
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
  *
- * @parent: pointer to the node to insert the right-child in
- * @value: value to store in the new node
+ * @parent: Pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
  *
- * Return: pointer to the new node, or NULL on failure
+ * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Check if parent is NULL */
-    if (parent == NULL)
-        return (NULL);
+	/* Check if parent is NULL */
+	if (parent == NULL)
+		return (NULL);
 
-    /* Allocate memory for new node */
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for new node */
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Insert new node as right-child */
-    if (parent->right != NULL)
-    {
-        parent->right->parent = new_node;
-        new_node->right = parent->right;
-    }
-    parent->right = new_node;
+	/* Insert new node as right-child */
+	if (parent->right != NULL)
+	{
+		parent->right->parent = new_node;
+		new_node->right = parent->right;
+	}
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
 
 /**
- * binary_tree_delete - deletes an entire binary tree
+ * binary_tree_delete - Deletes an entire binary tree
  *
- * @tree: pointer to the root node of the tree to delete
+ * @tree: Pointer to the root node of the tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree != NULL)
-    {
-        binary_tree_delete(tree->left);
-       
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the size of
+ *
+ * Return: The size of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
