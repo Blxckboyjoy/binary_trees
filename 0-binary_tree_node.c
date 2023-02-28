3b0d1fd0bd533aadb4f2e8fb76183e5380c4f5bb
Natangwe <natangwetj@gmail.com>
2023-02-28 08:06:28 -0900

    fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,120 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree node
+ * heap_to_sorted_array - Converts a binary max heap to a sorted array
+ * @heap: Pointer to the root node of the heap to convert
+ * @size: Address to store the size of the array
  *
- * @parent: Pointer to the parent node of the node to create
- * @value: Value to put in the new node
- *
- * Return: Pointer to the new node, or NULL on failure
+ * Return: Pointer to the sorted array, or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	binary_tree_t *new_node;
+    int *sorted_array;
+    size_t i;
 
-	/* Allocate memory for new node */
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
-		return (NULL);
+    if (heap == NULL || size == NULL)
+        return (NULL);
 
-	/* Initialize node */
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
+    sorted_array = malloc(heap->size * sizeof(int));
+    if (sorted_array == NULL)
+        return (NULL);
 
-	return (new_node);
-}
+    for (i = 0; i < heap->size; i++)
+        sorted_array[i] = heap_extract(&heap);
 
-/**
- * binary_tree_insert_left - Inserts a node as the left-child of another node
- *
- * @parent: Pointer to the node to insert the left-child in
- * @value: Value to store in the new node
- *
- * Return: Pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node;
-
-	/* Check if parent is NULL */
-	if (parent == NULL)
-		return (NULL);
-
-	/* Allocate memory for new node */
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
-
-	/* Insert new node as left-child */
-	if (parent->left != NULL)
-	{
-		parent->left->parent = new_node;
-		new_node->left = parent->left;
-	}
-	parent->left = new_node;
-
-	return (new_node);
-}
-
-/**
- * binary_tree_insert_right - Inserts a node as the right-child of another node
- *
- * @parent: Pointer to the node to insert the right-child in
- * @value: Value to store in the new node
- *
- * Return: Pointer to the new node, or NULL on failure
- */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new_node;
-
-	/* Check if parent is NULL */
-	if (parent == NULL)
-		return (NULL);
-
-	/* Allocate memory for new node */
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-		return (NULL);
-
-	/* Insert new node as right-child */
-	if (parent->right != NULL)
-	{
-		parent->right->parent = new_node;
-		new_node->right = parent->right;
-	}
-	parent->right = new_node;
-
-	return (new_node);
-}
-
-/**
- * binary_tree_delete - Deletes an entire binary tree
- *
- * @tree: Pointer to the root node of the tree to delete
- */
-void binary_tree_delete(binary_tree_t *tree)
-{
-	if (tree != NULL)
-	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
-	}
-}
-
-/**
- * binary_tree_size - Measures the size of a binary tree
- *
- * @tree: Pointer to the root node of the tree to measure the size of
- *
- * Return: The size of the tree, or 0 if tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+    *size = i;
+    return (sorted_array);
 }
